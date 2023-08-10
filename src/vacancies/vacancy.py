199f2507568c86a7bb41121c5c20f2c7604ff441
u_alex90 <u_alex90@mail.ru>
2023-08-10 22:49:40 +1000

    mod Vacancy

--- src/vacancies/vacancy.py
+++ src/vacancies/vacancy.py
@@ -1,6 +1,7 @@
 class Vacancy:
 
-    __slots__ = ('vacancy_id', 'service', 'title', 'date', 'area', 'currency', 'salary_fom', 'salary_to', 'url')
+    __slots__ = ('vacancy_id', 'service', 'title', 'date', 'area', 'currency',
+                 'salary_fom', 'salary_to', 'url', 'is_favorite', 'is_to_removed')
 
     def __init__(self, vacancy_id: str, service: str, title: str, date: str, area: str,
                  currency: str, salary_fom: int, salary_to: int, url: str) -> None:
@@ -16,6 +17,8 @@ class Vacancy:
         self.salary_fom = salary_fom
         self.salary_to = salary_to
         self.url = url
+        self.is_favorite = False
+        self.is_to_removed = False
 
     def __str__(self):
         return f'''Название: "{self.title}"
